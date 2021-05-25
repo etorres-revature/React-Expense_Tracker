05035df3823ec593b75e50b83c57d1a8d2cf9104
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-25 11:27:52 -0500

    wrapper card component for expenses

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,14 +1,16 @@
 import React from "react";
 
+import "./ExpenseDate.css";
+
 function ExpenseDate(expenses) {
   const month = expenses.date.toLocaleString("en-US", { month: "long" });
   const year = expenses.date.getFullYear();
   const day = expenses.date.toLocaleString("en-US", { day: "2-digit" });
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import React from "react";
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
 
 function ExpenseItem(expenses) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={expenses.date} />
       <div className="expense-item__description">
         <h2>{expenses.title}</h2>
         <div className="expense-item__price">${expenses.amount} </div>
       </div>
-    </div>
+    </Card>
   );
 }
 
