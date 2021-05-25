6f3ee544fd3fc59e7fec1fc175cfce5b030fdc03
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-25 13:49:58 -0500

    filter added

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,25 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
 
 const ExpenseItem = (expenses) => {
-  const [title, setTtitle] = useState(expenses.title);
-
-  const clickHandler = () => {
-    setTtitle("Joe Mama!!");
-    console.log("the title has been updated to", title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={expenses.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{expenses.title}</h2>
         <div className="expense-item__price">${expenses.amount} </div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
