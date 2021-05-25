e335afa1f4cb875a0e825a67b48645513dcfca51
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-25 13:05:38 -0500

    basic form functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Expenses from "./components/expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
   const expenses = [
@@ -24,8 +25,7 @@ const App = () => {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible...</p>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
 
 const ExpenseItem = (expenses) => {
+  const [title, setTtitle] = useState(expenses.title);
+
+  const clickHandler = () => {
+    setTtitle("Joe Mama!!");
+    console.log("the title has been updated to", title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={expenses.date} />
       <div className="expense-item__description">
-        <h2>{expenses.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${expenses.amount} </div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
